e216062756e550ed1dcb63366766a0d549b68f9e
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 07:06:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(strings_array)
   if strings_array.length == 2 
     return strings_array.join(" and ")
   elsif strings_array.length >=  3
-  
+    string = strings_array.join(" ")
     strings_array.insert(-2, "and")
     string = strings_array.join(" ")
   else
