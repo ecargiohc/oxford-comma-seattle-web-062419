2e00350356de11a3c217ff740969f9fd424b2d05
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:54:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(strings_array)
   strings_array.join(" ,")
+  if strings_array.length == 2 
+    return strings_array.join("and")
+    elsif strings_array.length > 3
+    return strings_array.each do |string| string[-1].join("and")
   
 end
 
 
-# return ["kiwi"].join 
-  
-#   return ["kiwi", "durian"].join("and")
-  
-#   #return ["kiwi", "durian", "starfruit"].join()
-#   # 
