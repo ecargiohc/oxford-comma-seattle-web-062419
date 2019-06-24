a8700befd229ae474c87d05a5968307842414ef8
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:58:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(strings_array)
   if strings_array.length == 2 
     return strings_array.join(" and ")
-  # elsif strings_array.length > 3
-  #   return strings_array.each do |string| string[-1].join("and")
+  elsif strings_array.length > 3
+    return strings_array
   else
     strings_array.join(" ,")
   end
