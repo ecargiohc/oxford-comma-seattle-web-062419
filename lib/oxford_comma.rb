113f900df417318376060afaaf6fd8f6dde6dbf6
Grace Colette <ilovegrace09@gmail.com>
2019-06-24 06:59:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(strings_array)
   if strings_array.length == 2 
     return strings_array.join(" and ")
   elsif strings_array.length > 3
-    strings_array.join(" ") = string
-    return strings_array.insert(-1, "and")
+    string = strings_array.join(" ")
+    return string.insert(-1, "and")
   else
     strings_array.join(" ,")
   end
